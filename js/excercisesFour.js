d7d14d0543b7e978381cc71bea13dcb1454862d7
linkin <pablogermanjimenez7@gmail.com>
2025-06-24 16:36:13 -0300

    creation excercises five

--- js/excercisesFour.js
+++ js/excercisesFour.js
@@ -1,2 +1,13 @@
 /*4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.*/
 
+const userText = parseInt(prompt('Give me a integer number and we will if its even or odd'));
+console.log(userText)
+function verifyEvenOdd(userText){
+    if(userText%2===0){
+        return document.writeln(`The result is even!`)
+    }else{
+        return document.writeln(`The result is Odd!`)
+    }
+}
+
+verifyEvenOdd()
