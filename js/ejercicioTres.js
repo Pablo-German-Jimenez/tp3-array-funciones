0ebf5b58a2eaf2f835a3e49606db5f36f7fbbd52
linkin <pablogermanjimenez7@gmail.com>
2025-06-24 15:22:07 -0300

    ejercicio tres

--- js/ejercicioTres.js
+++ js/ejercicioTres.js
@@ -1,7 +1,7 @@
 /*3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
 
 */
-const dadoUno = prompt('Decime tu nombre para tirar el primer dados!');
+const dadoUno = prompt(`Ameo tenes hora? no te persiga gato`);
 const dadoDos = prompt('Y ahora decime el nombre del segundo jugador! (jugaba alone estaba más solo que loco malo! jajaja)')
 const resultadoRandom = [];
 const lanzamientoDados = [];
