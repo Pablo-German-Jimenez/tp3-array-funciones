58faa1e834ca9d14b3c9d586f62d1bd200123059
linkin <pablogermanjimenez7@gmail.com>
2025-06-26 09:58:26 -0300

    creation and done of excersice six

---
+++ js/excercisesSix.js
@@ -0,0 +1,18 @@
+/*6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+⦁	La fórmula del perímetro  es p = 2*(a +b)
+Ejemplo:
+Input:
+lado A = 24
+lado B = 5
+Output: 58*/
+
+
+const sideRectangleOne = parseInt(prompt('give me the value of a side reactangle'));
+const sideRectangleTwo =  parseInt(prompt('and now give me the value of the other side of the reactangle!'));
+
+
+function calculatePerimeter(sideRectangleOne,sideRectangleTwo){
+ return perimeter = 2*(sideRectangleOne+sideRectangleTwo);
+}
+
+alert(`The value of the rectangle is ${calculatePerimeter(sideRectangleOne,sideRectangleTwo)}`);
